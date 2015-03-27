d14c7b8a5dae167cbb583209e3ef35c5d48ff5bb
Victor Guerra <vguerra@gmail.com>
2015-03-27 21:06:47 +0100

    Adding gitignore file. Adding all UI Elements to the scene: Zip Code TextField, Cash Text Field and Switch-activated Text Field.

--- DelegateChallenge/ViewController.swift
+++ DelegateChallenge/ViewController.swift
@@ -10,14 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var zipCodeTextField: UITextField!
+
+    let zipCodeDelegate = ZipCodeTextFieldDelegate()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        zipCodeTextField.delegate = zipCodeDelegate
     }
 
 
