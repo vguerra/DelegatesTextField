6fad82766353bebf1b1fbebb5d1913a537316dea
Victor Guerra <vguerra@gmail.com>
2015-03-28 10:48:17 +0100

    Implemented delegate for locked TextField. The editing is controlled by a Switch.

--- DelegateChallenge/ViewController.swift
+++ DelegateChallenge/ViewController.swift
@@ -8,21 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var zipCodeTextField: UITextField!
     @IBOutlet weak var cashTextField: UITextField!
-
+    @IBOutlet weak var lockTextFieldSwitch: UISwitch!
+    @IBOutlet weak var lockedTextField: UITextField!
+    
     let zipCodeDelegate = ZipCodeTextFieldDelegate()
     let cashDelegate = CashTextFieldDelegate()
     
     override func viewDidLoad() {
+    
         super.viewDidLoad()
 
         zipCodeTextField.delegate = zipCodeDelegate
         cashTextField.delegate = cashDelegate
+        lockedTextField.delegate = self
     }
 
-
+    @IBAction func switchEditing(sender: UISwitch) {
+        if !sender.on {
+            lockedTextField.resignFirstResponder()
+        }
+    }
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        return lockTextFieldSwitch.on
+    }
 }
 
